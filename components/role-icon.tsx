341e0d83f8b1960061c8a27c66c3762a113ef1d6
Dlean Jeans <12462089+DleanJeans@users.noreply.github.com>
2026-07-18 03:48:31 +0700

    RoleIcon: Increase image scale

--- components/role-icon.tsx
+++ components/role-icon.tsx
@@ -15,7 +15,7 @@ export function RoleIcon({ role, size = 24 }: { role: Role; size?: number }) {
       accessible={false}
       resizeMode="cover"
       source={{ uri: getRoleIconUrl(role) }}
-      style={{ borderRadius: size / 2, height: size, width: size }}
+      style={{ borderRadius: size / 2, height: size, transform: [{ scale: 1.2 }], width: size }}
     />
   );
 }
